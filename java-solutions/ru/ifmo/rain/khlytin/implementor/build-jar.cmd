9ea041bbc7d3e88da87e0b40ec12b9be6a5aa11e
khlytin <xlootin@niuitmo.ru>
2020-05-21 03:20:40 +0300

    HW update.

--- java-solutions/ru/ifmo/rain/khlytin/implementor/build-jar.cmd
+++ java-solutions/ru/ifmo/rain/khlytin/implementor/build-jar.cmd
@@ -1,7 +1,4 @@
 @ECHO off
-
-SET initDir=%CD%
-
 SET pathToThisScript=%~dp0
 SET pathToThisScript=%pathToThisScript:~0,-1%
 cd %pathToThisScript%
@@ -10,19 +7,14 @@ SET externalFolder=%CD%
 cd %pathToThisScript%
 cd ..\..\..\..\..\
 SET src=%CD%
-cd %pathToThisScript%
-
-SET outFolder=%pathToThisScript%\_build
 SET artifacts=%externalFolder%\java-advanced-2020\artifacts
 SET lib=%externalFolder%\java-advanced-2020\lib
 SET allLib=%lib%;%artifacts%
-
-SET modPath=ru\ifmo\rain\khlytin\implementor
-SET modName=ru.ifmo.rain.khlytin.implementor
-
+SET classpath=ru\ifmo\rain\khlytin\implementor
+SET classname=ru.ifmo.rain.khlytin.implementor
+SET outFolder=%pathToThisScript%\_build
+cd %pathToThisScript%
 mkdir "%outFolder%"
-javac --module-path "%allLib%" "%src%"\module-info.java "%src%"\%modPath%\*.java -d "%outFolder%\%modName%"
-cd "%outFolder%\%modName%"
-jar --create --file="%pathToThisScript%"\_implementor.jar --main-class=%modName%.JarImplementor --module-path="%allLib%" module-info.class %modPath%\*.class
-
-cd "%initDir%"
+javac --module-path "%allLib%" "%src%"\module-info.java "%src%"\%classpath%\*.java -d "%outFolder%\%classname%"
+cd "%outFolder%\%classname%"
+jar --create --file="%pathToThisScript%"\_implementor.jar --main-class=%classname%.JarImplementor --module-path="%allLib%" module-info.class %classpath%\*.class
--- java-solutions/ru/ifmo/rain/khlytin/implementor/compile-javadoc.cmd
+++ java-solutions/ru/ifmo/rain/khlytin/implementor/compile-javadoc.cmd
@@ -1,23 +1,15 @@
 @ECHO off
-
-SET initDir=%CD%
-
 SET pathToThisScript=%~dp0
 SET pathToThisScript=%pathToThisScript:~0,-1%
 cd %pathToThisScript%
 cd ..\..\..\..\..\..\..\
 SET externalFolder=%CD%
-cd %pathToThisScript%
-
 SET artifacts=%externalFolder%\java-advanced-2020\artifacts
 SET lib=%externalFolder%\java-advanced-2020\lib
 SET modules=%externalFolder%\java-advanced-2020\modules
-
-SET modPath=ru\ifmo\rain\khlytin\implementor
-SET modName=ru.ifmo.rain.khlytin.implementor
 SET implerPath=info\kgeorgiy\java\advanced\implementor
 SET implerName=info.kgeorgiy.java.advanced.implementor
-
+cd %pathToThisScript%
 javadoc ^
     -private ^
     -link https://docs.oracle.com/en/java/javase/11/docs/api/ ^
@@ -28,5 +20,3 @@ javadoc ^
      "%modules%\%implerName%\%implerPath%\JarImpler.java" ^
      "%modules%\%implerName%\%implerPath%\ImplerException.java" ^
      "%modules%\%implerName%\%implerPath%\package-info.java"
-
-cd "%initDir%"
