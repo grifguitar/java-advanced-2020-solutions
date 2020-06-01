509736a518dbdf0eaad303dd9a5455fbddd7d1c3
khlytin <xlootin@niuitmo.ru>
2020-06-01 17:57:49 +0300

    HW13 update scripts.

--- java-solutions/ru/ifmo/rain/khlytin/i18n/runProject.cmd
+++ java-solutions/ru/ifmo/rain/khlytin/i18n/runProject.cmd
@@ -1,10 +1,12 @@
 @ECHO off
 SET initPath=%~dp0
 SET initPath=%initPath:~0,-1%
-cd ..\..\..\..\..\..\..\
+cd %initPath%
+cd ..\..\..\..\..\
+SET javaSolutions=%CD%
+cd ..\..\
 SET src=%CD%
 SET lib=%src%\java-advanced-2020\lib
-SET javaSolutions=%src%\java-advanced-2020-solutions\java-solutions
 SET classpath=ru\ifmo\rain\khlytin\i18n
 SET classname=ru.ifmo.rain.khlytin.i18n
 SET allLib=%lib%\junit-4.11.jar;%lib%\hamcrest-core-1.3.jar
